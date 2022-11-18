fcc03b30312036c911fd5c6fdd7703c08e92f792
sairessy <sairessy@gmail.com>
2022-11-18 09:06:16 -1100

    Remove socket

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import users from "./dev/index.js";
 import express from "express";
 import cors from "cors";
 import http from "http";
-import { Server } from "socket.io";
+// import { Server } from "socket.io";
 
 const app = express();
 app.use(cors({ origin: "*", allowedHeaders: "*" }));
@@ -19,16 +19,16 @@ app.use("/", main);
 app.use("/dev", users);
 
 // WebSocket
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-});
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*",
+//   },
+// });
 
-io.on("connection", (socket) => {
-  socket.on("devices-camera-img", (data) => {
-    io.emit("devices-camera-img", data);
-  });
-});
+// io.on("connection", (socket) => {
+//   socket.on("devices-camera-img", (data) => {
+//     io.emit("devices-camera-img", data);
+//   });
+// });
 
 export default server;
