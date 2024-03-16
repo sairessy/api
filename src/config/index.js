025283a9310631a596cba77138f359072bd56da9
sairessy <sairessy@gmail.com>
2024-03-16 11:55:50 +0200

    envi

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 import { fileURLToPath } from 'url';
 import path from 'path';
 
-export const env = dotenv.config().parsed;
+export const env = process.env.EMAIL ? process.env : dotenv.config().parsed;
 export const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
