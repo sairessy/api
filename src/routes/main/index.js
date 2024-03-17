2de85011e5dcbabdfd809a8f8aebdbdfa5f58aad
sairessy <sairessy@gmail.com>
2024-03-17 09:26:29 +0200

    Setup cron

--- src/routes/main/index.js
+++ src/routes/main/index.js
@@ -5,10 +5,15 @@ import {
   home
 } from "../../controllers/main/index.js";
 import log from '../../middleware/main/log.js';
+import sendMail from '../../services/nodemailer/send_mail.js';
 
 const router = express.Router();
 
 router.get("/", log, home);
+router.get("/cron", (req, res) => {
+  sendMail(['sairessy@gmail.com'], 'Cron', `Hello, it is ${new Date()}.`);
+  res.json({data: {}});
+});
 router.get("/maputo/bairros/:distrito", log, getBairros);
 router.get("/maputo/distritos", log, getDistritos);
 
