69e7eb9fe2fabd3cdb32b19b67ec7d7daf262aa7
sairessy <sairessy@gmail.com>
2024-04-02 07:34:30 +0200

    nodemailer better

--- src/routes/main/index.js
+++ src/routes/main/index.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.get("/", log, home);
 router.get("/cron", async (req, res) => {
   const r = await sendMail(['sairessy@gmail.com'], 'Cron', `Hello, it is ${new Date()}, it comes from ${req.get('host')}.`);
-  res.json({data: {data: req.get('host')}});
+  res.json({data: r});
 });
 router.get("/maputo/bairros/:distrito", log, getBairros);
 router.get("/maputo/distritos", log, getDistritos);
