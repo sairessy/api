8a2dd76ff78e42eadb5431c8c7425690ab7fffbc
sairessy <sairessy@gmail.com>
2024-04-02 06:47:05 +0200

    nodemailer problems on remote - cyclic

--- src/controllers/chatbot/foco.js
+++ src/controllers/chatbot/foco.js
@@ -2,11 +2,11 @@ import { NlpManager } from 'node-nlp';
 
 const manager = new NlpManager({ languages: ['en'], forceNER: true });
 
-manager.addDocument('en', 'courses', 'courses.show');
-manager.addDocument('en', 'duration', 'courses.duration')
+manager.addDocument('en', 'cursos', 'mostra.cursos');
+manager.addDocument('en', 'dura duracao', 'mostra.duracao');
 
-manager.addAnswer('en', 'courses.show', 'A Nossa instituicao oferece os seguintes cursos: CA, MI, GG, GRH, EI, ADU, EDINF e CC');
-manager.addAnswer('en', 'courses.duration', 'Todos os nossos cursos tem a duracao de 3 anos, com excessao do curso de EINF que tem uma duracao de 3 meses');
+manager.addAnswer('en', 'mostra.cursos', 'CA, GG');
+manager.addAnswer('en', 'mostra.duracao', 'Todos cursos duram 3 anos, com excessao de EINF, que dura um ano.')
 
 export default async function reply(text) {
   await manager.train();
--- src/routes/main/index.js
+++ src/routes/main/index.js
@@ -10,9 +10,9 @@ import sendMail from '../../services/nodemailer/send_mail.js';
 const router = express.Router();
 
 router.get("/", log, home);
-router.get("/cron", (req, res) => {
-  sendMail(['sairessy@gmail.com'], 'Cron', `Hello, it is ${new Date()}.`);
-  res.json({data: {}});
+router.get("/cron", async (req, res) => {
+  const r = await sendMail(['sairessy@gmail.com'], 'Cron', `Hello, it is ${new Date()}.`);
+  res.json({data: {data: r}});
 });
 router.get("/maputo/bairros/:distrito", log, getBairros);
 router.get("/maputo/distritos", log, getDistritos);
