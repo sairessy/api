5f8130c55b90a05fb3c26f61bcc8372d0f00c526
sairessy <sairessy@gmail.com>
2024-06-12 10:14:13 +0200

    Basic fix on Cronus controller

--- src/controllers/chatbot/index.js
+++ src/controllers/chatbot/index.js
@@ -3,7 +3,7 @@ import { NlpManager } from 'node-nlp';
 const manager = new NlpManager();
 
 export const chat = async (req, res) => {
-  const model = `./src/node-nlp-models/${req.headers.org}.nlp`;
+  const model = `./src/node-nlp-models/${req.headers.bot}.nlp`;
   manager.load(model);
   const r = await manager.process('en', req.body.text);
   res.json(r);
