8b89a1e52ee9fc9d5af265fe38cdaa0af0f1978b
sairessy <sairessy@gmail.com>
2024-07-04 12:21:47 -0700

    Made cronus delete data

--- src/controllers/cronus/index.js
+++ src/controllers/cronus/index.js
@@ -1,9 +1,50 @@
-import { houses, tecnicos } from "../../config/index.js";
+import { Staff } from "../../models/cronus/staff.js";
+import { Stock } from "../../models/cronus/stock.js";
+import { Credito } from "../../models/cronus/credito.js";
+import { Asset } from "../../models/cronus/asset.js";
 
-export const getHouses = async (req, res) => {
-  res.json(houses);
-};
+export const removeItem = async (req, res) => {
+  const { id, item } = req.body;
+  const user = req.headers.user;
+  console.log(id, item, user);
 
-export const getTechs = async (req, res) => {
-  res.json(tecnicos);
+  if (item === "staff") {
+    const data = await Staff.findOneAndUpdate(
+      { _id: id },
+      { removed: true },
+      { new: true }
+    );
+
+    res.json(data);
+  }
+
+  if (item === "stock") {
+    const data = await Stock.findOneAndUpdate(
+      { _id: id },
+      { removed: true },
+      { new: true }
+    );
+
+    res.json(data);
+  }
+
+  if (item === "asset") {
+    const data = await Asset.findOneAndUpdate(
+      { _id: id },
+      { removed: true },
+      { new: true }
+    );
+
+    res.json(data);
+  }
+
+  if (item === "credito") {
+    const data = await Credito.findOneAndUpdate(
+      { _id: id },
+      { removed: true },
+      { new: true }
+    );
+
+    res.json(data);
+  }
 };
--- src/routes/cronus/index.js
+++ src/routes/cronus/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 
+import { removeItem } from "../../controllers/cronus/index.js";
 import { createStaff, createStaffCategoria, getStaff, getStaffCategoria,  } from "../../controllers/cronus/Staff.js";
 import { createCredito, getCredito } from "../../controllers/cronus/Credito.js";
 import { createStock, getStock, createStockCategoria, getStockCategoria } from "../../controllers/cronus/Stock.js";
@@ -8,6 +9,9 @@ import { createAsset, createAssetCategoria, getAsset, getAssetCategoria } from "
 const router = express.Router();
 
 router.get("/", (req, res) => res.json({ app: "CRONUS" }));
+
+// Remove
+router.delete("/remove", removeItem);
 
 // Staff
 router.get("/staff/", getStaff);
